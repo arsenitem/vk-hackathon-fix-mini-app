e9d1f8a872e3f02f7c13b21e7acf23892dac71e2
arsenitem <arsenitem@bk.ru>
2020-09-15 00:29:38 +0500

    Updates

---
+++ precache-manifest.acccf4f24a47984cde52d6ea16e7ec69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392cf7feafd9257268b3326ae0c34a44",
+    "url": "/vk-hackathon-fix-mini-app/index.html"
+  },
+  {
+    "revision": "6f6a2785fdacae5bba0c",
+    "url": "/vk-hackathon-fix-mini-app/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "6f6a2785fdacae5bba0c",
+    "url": "/vk-hackathon-fix-mini-app/static/js/2.8db62f1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk-hackathon-fix-mini-app/static/js/2.8db62f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "348e873758532826a180",
+    "url": "/vk-hackathon-fix-mini-app/static/js/main.8418c22e.chunk.js"
+  },
+  {
+    "revision": "b5983c3e1074ca4ae482",
+    "url": "/vk-hackathon-fix-mini-app/static/js/runtime-main.be17de29.js"
+  }
+]);
